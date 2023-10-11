539b9f3f5c320b5098dd06fa2a35755c8f3062fd
Nastasa Baras Luca <66071357+Lucanb@users.noreply.github.com>
2023-10-11 14:12:39 +0300

    ex1 done

--- Lab_1/Ex_1.py
+++ Lab_1/Ex_1.py
@@ -1,16 +1,45 @@
 # This is a sample Python script.
+import numpy as np
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def calculate_gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
 
+def calculate_multiple_cmmdc():
+    elements = []
+    while True:
+        num_input = input("Enter numbers or 'exit to quit'")
+        if num_input == 'exit':
+            break
+        if num_input.isdigit():
+            number = int(num_input)
+            if number > 0:
+                elements.append(number)
+            else:
+                print("Error: Please enter natural number.")
+        else:
+            print("Error: Please enter natural number.")
+    if elements:
+       prev = elements[0]
+       gcd = prev
+       for element in elements[1:]:
+        gcd = calculate_gcd(element,prev)
+        if gcd == 1:
+            print(gcd)
+            return
+        prev = gcd
+       print(gcd)
+    else:
+        print("you didn't enter the numbers!")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    calculate_multiple_cmmdc()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
