1e6cc35b140999c9b6e8f3f1beb628bdc5d980d0
Nastasa Baras Luca <66071357+Lucanb@users.noreply.github.com>
2023-10-11 14:39:03 +0300

    Ex4

---
+++ Lab_1/Ex_4.py
@@ -0,0 +1,8 @@
+import  numpy as np
+
+def new_case(str):
+    return ''.join(['_' + i.lower() if i.isupper()
+                    else i for i in str]).lstrip('_')
+
+if __name__ == '__main__':
+    new_case("AmMersLa")
