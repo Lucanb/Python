0af190178ba63a47e1446bb7bf7d8278777da8ad
Lucanb <lucanastasa@yahoo.com>
2023-10-25 17:03:59 +0300

    done

--- Lab_3/Ex_3.py
+++ Lab_3/Ex_3.py
@@ -1,38 +1,39 @@
 
-def Compare_Dictionaries(dict_a,dict_b):
+def Compare_Dictionaries(dict_a, dict_b):
+    if type(dict_a) != type(dict_b):
+        return False
 
-    if(set(dict_a)!=set(dict_b)):
-      return False
-    
-    for key in dict_a:
-       type_a = type(dict_a[key])
-       type_b = type(dict_b[key])
+    if isinstance(dict_a, dict):
+        if set(dict_a.keys()) != set(dict_b.keys()):
+            return False
 
-       if type_a in (int, float, str, bool):
-            if dict_a[key] != dict_b[key]:
+        for key in dict_a:
+            if not Compare_Dictionaries(dict_a[key], dict_b[key]):
                 return False
-            elif type_a in (list, tuple, set):
-             if len(dict_a[key]) != len(dict_b[key]):
+
+    elif isinstance(dict_a, (list, tuple, set)):
+        if len(dict_a) != len(dict_b):
+            return False
+
+        for item_a, item_b in zip(dict_a, dict_b):
+            if not Compare_Dictionaries(item_a, item_b):
                 return False
-             for i in range(len(dict_a[key])):
-                if not Compare_Dictionaries(dict_a[key][i], dict_b[key][i]):
-                    return False
-            elif type_a is dict:
-                 if not Compare_Dictionaries(dict_a[key], dict_b[key]):
-                    return False
+
+    else:
+        if dict_a != dict_b:
+            return False
 
     return True
 
+if __name__ == '__main':
+    dict1 = {"a": 1, "b": [2, 3, {"c": 4}], "d": {"e": 5}}
+    dict2 = {"a": 1, "b": [2, 3, {"c": 4}], "d": {"e": 5}}
+    dict3 = {"a": 1, "b": [2, 3, {"c": 4}], "d": {"e": 7}}
 
-
-
-if __name__ == '__main__':
-  dict1 = {"a": 1, "b": [2, 3, {"c": 4}], "d": {"e": 5}}
-  dict2 = {"a": 1, "b": [2, 3, {"c": 4}], "d": {"e": 5}}
-
-result1 = Compare_Dictionaries(dict1, dict2)
-
-print(result1)
+    result1 = Compare_Dictionaries(dict1, dict2)
+    result2 = Compare_Dictionaries(dict1, dict3)
+    print(result1)
+    print(result2)  
 
 
 
